5718a56630702dce3e0f9df3ba6399c0b6965e2d
Marcus Paredes <marcus.paredes@coopersystem.com.br>
2024-09-06 16:59:59 -0300

    🐛 Update gurnicorn configs

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -11,9 +11,11 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExport
 from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 
 SIGNOZ_ADDR = os.getenv("SIGNOZ_ADDR")
+OTEL_EXPORTER_OTLP_ENDPOINT = os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
 
 
 def post_fork(server, worker):
+    print(f"post_fork\nendpoint: {OTEL_EXPORTER_OTLP_ENDPOINT}\n")
     server.log.info("Worker spawned (pid: %s)", worker.pid)
     resource = Resource.create(attributes={
         "service.name": "pcc-platform-coop-directory",
@@ -22,15 +24,17 @@ def post_fork(server, worker):
     provider = TracerProvider(resource=resource)
 
     trace.set_tracer_provider(provider)
-    otlp_span_processor = BatchSpanProcessor(
-        OTLPSpanExporter(endpoint=os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT'))
-    )
-    console_span_processor = BatchSpanProcessor(ConsoleSpanExporter())
+    otlp_span_processor = BatchSpanProcessor(OTLPSpanExporter(
+        endpoint=os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT')
+    ))
+    console_span_processor = BatchSpanProcessor(ConsoleSpanExporter(
+        endpoint=os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT')
+    ))
 
     trace.get_tracer_provider().add_span_processor(console_span_processor)
     trace.get_tracer_provider().add_span_processor(otlp_span_processor)
 
-    LoggingInstrumentor().instrument(trace_provider=provider, set_logging_format=True)
-    DjangoInstrumentor().instrument(trace_provider=provider, is_sql_commentor_enabled=True)
-    Psycopg2Instrumentor().instrument(trace_provider=provider, skip_dep_check=True, enable_commenter=True)
-    RequestsInstrumentor().instrument(trace_provider=provider)
+    LoggingInstrumentor().instrument(set_logging_format=True)
+    DjangoInstrumentor().instrument(is_sql_commentor_enabled=True)
+    Psycopg2Instrumentor().instrument(skip_dep_check=True, enable_commenter=True)
+    RequestsInstrumentor().instrument()
