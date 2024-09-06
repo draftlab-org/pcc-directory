57afec898791e563d64ee164d0e783a19b9f80aa
Marcus Paredes <marcus.paredes@coopersystem.com.br>
2024-09-06 17:08:23 -0300

    🐛 Update gurnicorn configs

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -5,36 +5,27 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExport
 from opentelemetry.instrumentation.django import DjangoInstrumentor
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 
-SIGNOZ_ADDR = os.getenv("SIGNOZ_ADDR")
 OTEL_EXPORTER_OTLP_ENDPOINT = os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
 
 
 def post_fork(server, worker):
     print(f"post_fork\nendpoint: {OTEL_EXPORTER_OTLP_ENDPOINT}\n")
     server.log.info("Worker spawned (pid: %s)", worker.pid)
-    resource = Resource.create(attributes={
-        "service.name": "pcc-platform-coop-directory",
-        "service.namespace": os.getenv("ENV", "dev"),
-    })
-    provider = TracerProvider(resource=resource)
 
+    provider = TracerProvider()
     trace.set_tracer_provider(provider)
-    otlp_span_processor = BatchSpanProcessor(OTLPSpanExporter(
-        endpoint=os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT')
-    ))
-    console_span_processor = BatchSpanProcessor(ConsoleSpanExporter(
-        endpoint=os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT')
-    ))
+
+    otlp_span_processor = BatchSpanProcessor(OTLPSpanExporter())
+    console_span_processor = BatchSpanProcessor(ConsoleSpanExporter())
 
     trace.get_tracer_provider().add_span_processor(console_span_processor)
     trace.get_tracer_provider().add_span_processor(otlp_span_processor)
 
-    LoggingInstrumentor().instrument(set_logging_format=True)
-    DjangoInstrumentor().instrument(is_sql_commentor_enabled=True)
-    Psycopg2Instrumentor().instrument(skip_dep_check=True, enable_commenter=True)
-    RequestsInstrumentor().instrument()
+    LoggingInstrumentor().instrument(tracer_provider=provider, set_logging_format=True)
+    DjangoInstrumentor().instrument(tracer_provider=provider, is_sql_commentor_enabled=True)
+    Psycopg2Instrumentor().instrument(tracer_provider=provider, skip_dep_check=True, enable_commenter=True)
+    RequestsInstrumentor().instrument(tracer_provider=provider)
