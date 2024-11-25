d54ea5192db3c96b37df9346723159ec43ac3f44
Marcus Paredes <marcus.paredes@coopersystem.com.br>
2024-11-25 13:07:06 -0300

    🐛Add CSRF_TRUSTED_ORIGINS to application settings

--- cmdi/settings.py
+++ cmdi/settings.py
@@ -37,6 +37,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # See https://stackoverflow.com/questions/30015462/django-ignoring-debug-value-when-i-use-os-environ-why
 DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '*').split(' ')
+
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*')
 if ALLOWED_HOSTS != '':
     ALLOWED_HOSTS = ALLOWED_HOSTS.split(' ')
